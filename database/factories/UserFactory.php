4c7f9fba27c10bd6a99e8b78b0e81dac382b7e46
Yokabeth Valdes <djb502660@gmail.com>
2026-05-31 17:07:46 -0500

    Actualiza la configuración de PHPUnit para que el valor de DB_PASSWORD sea vacío en entornos locales y se ajuste a 'password' en GitHub Actions. Además, se modifica el UserFactory para que la contraseña se almacene de forma segura utilizando Hash::make.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -20,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= 'password',
+            'password' => static::$password ??= Hash::make('password'),
             'role' => UserRole::Client,
             'phone' => fake()->numerify('099#######'),
             'status' => 'activo',
